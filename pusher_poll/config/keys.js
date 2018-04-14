e98e3d2e78b9c7bea5fb3c712fb5299d58b814af
JaskiratSinghSra <35119674+JaskiratSinghSra@users.noreply.github.com>
2018-04-14 03:46:00 -0400

    jnk

---
+++ pusher_poll/config/keys.js
@@ -0,0 +1,8 @@
+module.exports = {
+  mongoURI: 'mongodb://sra0013:password@ds139448.mlab.com:39448/pusherpoll',
+  pusherAppId: '509255',
+  pusherKey: '6c92509f316dfe2f28bd',
+  pusherSecret: 'f6284dd70616ed16423a',
+  pusherCluster: 'us2',
+  pusherEncrypted: true
+};
